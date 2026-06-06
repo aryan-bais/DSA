c64bf58838b44fc2ba24db102bce367eeea439bf
Aryan-bais <aryanbais14@gmail.com>
2026-06-06 20:28:06 +0530

    Add LeetCode 34 solution and update search implementation

---
+++ binary_search/leetcode34.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& arr, int target) {
+    int n=arr.size();
+    int start =0,end=n-1,first=-1 ,last=-1,mid;
+    while(start<=end){
+    mid=start+(end-start)/2;
+
+    if(arr[mid]==target){
+        first=mid;
+        end=mid-1;
+
+    }
+
+    else if(arr[mid]<target)
+        start=mid+1;
+    
+    else
+    end=mid-1;
+    
+    
+    }
+
+     start=0,end=n-1;
+
+     while(start<=end){
+    mid=start+(end-start)/2;
+
+    if(arr[mid]==target){
+        last=mid;
+        start=mid+1;
+
+    }
+
+    else if(arr[mid]<target)
+        start=mid+1;
+    
+    else
+    end=mid-1;
+    
+    
+    }
+
+    vector<int>a(2);
+    a[0]=first;
+    a[1]=last;
+
+    return a;
+    }
+};
