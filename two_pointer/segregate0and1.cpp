df743e485df3bc83d33582fb5192f17c957b56f1
Aryan-bais <aryanbais14@gmail.com>
2026-06-19 11:09:50 +0530

    Add two_pointer directory

---
+++ two_pointer/segregate0and1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int arr[]={1,0,1,1,0,1,0,0,1};
+    int n=9;
+    int start=0,end=n-1;
+    while(start<end){
+     
+        if(arr[start]==0){
+            start++;
+        }
+         else
+        
+         {
+            if(arr[end]==0)
+            {
+                swap(arr[start],arr[end]);
+                start++,end--;
+            }
+            else
+            end--;
+         }
+        
+    }
+     for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+};
