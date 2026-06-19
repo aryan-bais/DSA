f05f5dd6e4d7e31bbd62d75073b09661621e34cc
Aryan-bais <aryanbais14@gmail.com>
2026-06-19 16:17:41 +0530

    Add solutions for Leetcode 26 and 27

---
+++ two_pointer/leetcode26.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& arr) {
+        int i=0,j=1;
+        int n=arr.size();
+
+        while(j<n){
+            if(arr[i]!=arr[j]){
+                i++;
+                arr[i]=arr[j];
+            }
+            j++;
+        }
+        return i+1;
+    }
+};
