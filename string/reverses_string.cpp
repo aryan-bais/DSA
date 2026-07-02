53718727d7d2d70493f2bbf89d89d0665af353d6
Aryan-bais <aryanbais14@gmail.com>
2026-07-02 12:39:00 +0530

    Add reverses_string.cpp

---
+++ string/reverses_string.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+     string s="aryan";
+    
+ int start=0,end=s.size()-1;
+     while(start<end){
+        swap(s[start],s[end]);
+        start++,end--;
+     }
+
+     cout<<s;
+}
