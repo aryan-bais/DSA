38639e6f75edef19e18dd778dc918b66ff823cc1
Aryan-bais <aryanbais14@gmail.com>
2026-07-02 11:47:43 +0530

    Update basic.cpp and add append.cpp

---
+++ string/append.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s1="aryan",s2="bais";
+    string s3=s1+s2;
+    string s4=s1.append(s2);
+    cout<<s3<<s4;
+}
+
+
--- string/basic.cpp
+++ string/basic.cpp
@@ -1,9 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    char arr[]={'a','p','p','l','e'};
-    for (int i=0;i<5;i++)
-
-cout<<arr[i];
-    
+   string s;
+   cin>>s;
+   cout<<s;
 }
