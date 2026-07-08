fecd96b6d70ece38454d9718d1ca628f20b2d0a0
Aryan-bais <aryanbais14@gmail.com>
2026-07-08 11:28:20 +0530

    add in end

--- linkedlist/insert_at_end.cpp
+++ linkedlist/insert_at_end.cpp
@@ -1,1 +1,43 @@
-   
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node *next;
+
+    node(int value){
+        data=value;
+        next=NULL;
+    }
+};
+
+int main(){
+    node*head,*tail;
+    tail=head=NULL;
+    int arr[]={1,2,3,4,5,6};
+    for(int i=0;i<6;i++){
+
+    if(head==NULL){
+
+        head=new node(arr[i]);
+        tail=head;
+    }
+
+    else{
+ tail->next=new node(arr[i]);
+ tail=tail->next;
+
+    }
+}
+node*temp;
+temp=head;
+
+while(temp){
+    cout<<temp->data<<" ";
+    temp=temp->next;
+}
+
+
+
+}
