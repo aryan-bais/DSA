c2a6f0dfc77e4f8fb4330504d995517e15e493f4
Aryan-bais <aryanbais14@gmail.com>
2026-07-20 10:19:43 +0530

    update

--- linkedlist/delete_a_node.cpp
+++ linkedlist/delete_a_node.cpp
@@ -15,10 +15,32 @@ class node
 
 int main(){
 
-    node *head;
-    head=NULL;
-    int arr[]={1,2,3,4,5}
+    node *head,*tail;
+   tail=head=NULL;
+    int arr[]={1,2,3,4,5};
+    for(int i=0;i<5;i++){
+        if(head==NULL){
+            head=new node(arr[i]);
+            tail=head;
+        }
+        else{
+          tail->next=new node(arr[i]);
+           tail=tail->next;
+  
+        }
+    }
+   
+    if(head!=NULL){
+        node*temp=head;
+        head=head->next;
+        delete temp;
+    }
+       node *curr = head;
+    while (curr != NULL) {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
 
-    
+    return 0;
 
 }
