4d6a23de6726ccd7e06cd7628244e84a5bfc2a8f
Aryan-bais <aryanbais14@gmail.com>
2026-07-20 10:06:52 +0530

    update

---
+++ linkedlist/delete_a_node.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class node
+{
+    public:
+    int data;
+    node *next;
+
+    node(int value){
+        data=value;
+        next=NULL;
+    }
+
+};
+
+int main(){
+
+    node *head;
+    head=NULL;
+    int arr[]={1,2,3,4,5}
+
+    
+
+}
